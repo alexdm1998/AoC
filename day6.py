544123a041e670dd0703e732db1a717c5473cbe1
alex <alexdm1998@gmail.com>
2024-12-09 18:30:45 +0000

    Day 6 Part 2 Solution. (Somewhat efficient)

--- day6.py
+++ day6.py
@@ -15,56 +15,50 @@ for line in lab:
 #Part 1
 n = len(lab_map)
 m = len(lab_map[0])
-cy, cx = 0,0
+icy, icx = 0,0
 for height, string in enumerate(lab_map):
     for width, char in enumerate(string):
         if char in ["^",">", "v", ">"]:
-            cy, cx = height, width
+            icy, icx = height, width
 
 direcs = [[-1,0],[0,1],[1,0],[0,-1]]
-cd = 0
+icd = 0
 
 
 
+
+exited = 0
 counter = 0
 for i in range(n):
     for j in range(m):
-        print("Checking at: (" + str(i) + "," + str(j) + ")")
-        print("Current counter of obstacles: " + str(counter))
-
         historic = set()
 
-        if not lab_map[i][j] == "#" or not lab_map[i][j] == "^":
+        if not lab_map[i][j] == "#" and not lab_map[i][j] == "^":
             lab_map[i][j] = "#"
         else:
             continue
 
+        cy, cx, cd = icy, icx, icd
         while True:
             pcy = cy + direcs[cd][0]
             pcx = cx + direcs[cd][1]
-        
-            if pcy not in range(n) or pcx not in range(m):
-                break
             
-            next_tile = (cy, cx, cd)
-            if next_tile in historic:
+            if pcy not in range(n) or pcx not in range(m):
+                exited += 1
+                break
+
+            tile = (pcy, pcx, cd)
+            if tile in historic:
                 counter += 1
                 break
-            
+
             if lab_map[pcy][pcx] == "#":
-                historic.add((cy,cx,cd))
+                historic.add((cy, cx, cd))
                 cd = (cd + 1) % 4
             else:
                 cy = pcy
                 cx = pcx
-        
+
         lab_map[i][j] = "."
-        
-print(counter)
 
-
-
-
-
-
-
+print(counter) #1670
